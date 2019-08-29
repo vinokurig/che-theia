ec29fcb63bc0ef8e4b7f63fea5aa286d165c912c
Roman Nikitenko <rnikiten@redhat.com>
2019-08-29 21:12:05 +0300

    Change way of storage runtime info for che tasks
    Signed-off-by: Roman Nikitenko <rnikiten@redhat.com>

--- extensions/eclipse-che-theia-plugin-ext/src/plugin/che-task-impl.ts
+++ extensions/eclipse-che-theia-plugin-ext/src/plugin/che-task-impl.ts
@@ -8,17 +8,15 @@
  * SPDX-License-Identifier: EPL-2.0
  **********************************************************************/
 import { CheTask, CheTaskMain, PLUGIN_RPC_CONTEXT } from '../common/che-protocol';
-import { TaskRunner, Disposable, Task, TaskInfo, TaskExitedEvent, TaskConfiguration } from '@eclipse-che/plugin';
+import { TaskRunner, Disposable, TaskInfo, TaskExitedEvent, TaskConfiguration } from '@eclipse-che/plugin';
 import { RPCProtocol } from '@theia/plugin-ext/lib/common/rpc-protocol';
 
 export class CheTaskImpl implements CheTask {
     private readonly cheTaskMain: CheTaskMain;
     private readonly runnerMap: Map<string, TaskRunner>;
-    private readonly taskMap: Map<number, Task>;
     constructor(rpc: RPCProtocol) {
         this.cheTaskMain = rpc.getProxy(PLUGIN_RPC_CONTEXT.CHE_TASK_MAIN);
         this.runnerMap = new Map();
-        this.taskMap = new Map();
     }
     async registerTaskRunner(type: string, runner: TaskRunner): Promise<Disposable> {
         this.runnerMap.set(type, runner);
@@ -30,33 +28,17 @@ export class CheTaskImpl implements CheTask {
         };
     }
 
-    async $runTask(id: number, config: TaskConfiguration, ctx?: string): Promise<void> {
+    async $runTask(config: TaskConfiguration, ctx?: string): Promise<TaskInfo> {
         const runner = this.runnerMap.get(config.type);
         if (runner) {
-            const task = await runner.run(config, ctx);
-            this.taskMap.set(id, task);
+            return await runner.run(config, ctx);
         }
     }
 
-    async $killTask(id: number): Promise<void> {
-        const task = this.taskMap.get(id);
-        if (task) {
-            await task.kill();
-            this.taskMap.delete(id);
-        }
-    }
-
-    async $getTaskInfo(id: number): Promise<TaskInfo | undefined> {
-        const task = this.taskMap.get(id);
-        if (task) {
-            return task.getRuntimeInfo();
-        }
-    }
-
-    async $onTaskExited(id: number): Promise<void> {
-        const task = this.taskMap.get(id);
-        if (task) {
-            this.taskMap.delete(id);
+    async $killTask(taskInfo: TaskInfo): Promise<void> {
+        const runner = this.runnerMap.get(taskInfo.config.type);
+        if (runner) {
+            return await runner.kill(taskInfo);
         }
     }
 
--- plugins/task-plugin/src/task/che-task-runner.ts
+++ plugins/task-plugin/src/task/che-task-runner.ts
@@ -44,7 +44,7 @@ export class CheTaskRunner {
     /**
      * Runs a task from the given task configuration which must have a target property specified.
      */
-    async run(taskConfig: che.TaskConfiguration, ctx?: string): Promise<che.Task> {
+    async run(taskConfig: che.TaskConfiguration, ctx?: string): Promise<che.TaskInfo> {
         const { type, label, ...definition } = taskConfig;
         if (type !== CHE_TASK_TYPE) {
             throw new Error(`Unsupported task type: ${type}`);
@@ -77,20 +77,29 @@ export class CheTaskRunner {
             const execId = await terminal.processId;
 
             return {
-                kill: () => {
-                    throw new Error('Stopping a Che task currently is not supported.');
-                },
-                getRuntimeInfo: () =>
-                    ({
-                        taskId: STUB_TASK_ID,
-                        ctx: ctx,
-                        config: taskConfig,
-                        execId: execId
-                    })
+                taskId: STUB_TASK_ID,
+                ctx: ctx,
+                config: taskConfig,
+                execId: execId
             };
         } catch (error) {
             console.error('Failed to execute Che command:', error);
             throw new Error(`Failed to execute Che command: ${error.message}`);
+        }
+    }
+
+    /** Terminates a task based on the given info. */
+    async kill(taskInfo: che.TaskInfo): Promise<void> {
+        for (const terminal of theia.window.terminals) {
+            try {
+                const processId = await terminal.processId;
+                if (processId === taskInfo.execId) {
+                    terminal.sendText('\x03');
+                    return;
+                }
+            } catch (e) {
+                // allow to get process id for other terminals
+            }
         }
     }
 }
